7cde48958ab14d62b59f87a913c9f647aa185fb4
kabir <makkobir79@gmail.com>
2022-06-21 01:56:21 +0600

    card added

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -11,7 +11,24 @@ const Post = () => {
 
   return (
     <div>
-      <h1>All Post: {posts.length} </h1>
+      <h1 className="text-danger my-4">All Post: {posts.length} </h1>
+      <div className="container">
+        {posts.map((post) => (
+          <div key={post.id}>
+            <div className="row">
+              <div class="col-md-6 rounded-3 w-50 mx-auto gy-5">
+                <div class="card-body bg-light">
+                  <h5 class="card-title">{post.title}</h5>
+                  <p class="card-text">{post.body}</p>
+                  <span className="my-4 px-2 cursor-pointer text-center text-warning border rounded">
+                    {post.userId}
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
